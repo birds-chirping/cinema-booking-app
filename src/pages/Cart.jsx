b2321536a54d3f5853412c9c503d72bd62d70cef
birds-chirping <110572438+birds-chirping@users.noreply.github.com>
2023-12-15 18:49:47 +0200

    Cart: show subtotal, discount, total.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./cart.css";
 import happy_people from "../assets/img/2151024819.jpg";
 
 const Cart = ({ ticketsInCart, setTicketsInCart }) => {
+  const discountCode = useRef();
+  const [total, setTotal] = useState(null);
+
   const handleDeleteTicket = (id) => {
     const updatedCart = ticketsInCart.filter((ticket) => {
       return ticket.seatID != id;
@@ -15,7 +18,20 @@ const Cart = ({ ticketsInCart, setTicketsInCart }) => {
     setTicketsInCart(updatedCart);
   };
 
-  console.log(ticketsInCart);
+  const validateDiscountCode = () => {
+    return discountCode.current.value === "movieweek" ? 40 : 0;
+  };
+
+  const calculateSubtotal = (arr) => {
+    return arr.reduce((a, b) => a + Number(b.price), 0);
+  };
+
+  const calculateTotal = () => {
+    const discount = validateDiscountCode();
+    setTotal(ticketsInCart.reduce((a, b) => a + Number((b.price * (100 - discount)) / 100), 0).toFixed(2));
+  };
+
+  // console.log(ticketsInCart);
 
   return (
     <div className="cart-page">
@@ -91,7 +107,18 @@ const Cart = ({ ticketsInCart, setTicketsInCart }) => {
                 );
               })}
             </div>
-            <div className="cart-info">{/* <div className="subtotal">Subtotal: {}</div> */}</div>
+            <div className="cart-info">
+              <div className="discount">
+                <label htmlFor="discount-code">If you have a coupon code, please enter it here</label>
+                <input ref={discountCode} type="text" id="discount-code" />
+                <button onClick={calculateTotal}>Apply</button>
+              </div>
+              <div className="subtotal">Subtotal: {calculateSubtotal(ticketsInCart).toFixed(2)} RON</div>
+              <div className="discount-sum">
+                Discount: {total > 0 ? (total - calculateSubtotal(ticketsInCart)).toFixed(2) : "0.00"} RON
+              </div>
+              <div className="total">Total: {total || calculateSubtotal(ticketsInCart).toFixed(2)} RON</div>
+            </div>
             <div className="cart-buttons">
               <div className="continue-shopping">
                 <button className="continue-shopping-button">Continue Shopping</button>
